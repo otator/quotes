634b95aa44a5c10c50806a0ac14fbcb064b6b9f7
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-04-13 15:55:01 +0300

    lab is done

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # quotes
+in this lab a [json file](app/src/main/resources/recentquotes.json) has been read and displayed one of its objects randomly
+
+### requirements
+the projects has the following classes:
+* [Quote.java](app/src/main/java/quotes/Quote.java) this class to create an object of type Quote that has a name and text.
+* [App.java](app/src/main/java/quotes/App.java) this class to read the json file, create quotes objects out of it and disply randomly quote
+* [AppTest.java](app/src/test/java/quotes/AppTest.java) this class to test the App class and it has one method `testJsonHasQuote()` it tests if a specific Quote object exists in the list of objects that had been read from the json file 
+
+the following methods had been created in the [App.java]((app/src/main/java/quotes/App.java)) class to make it work as the lab requires.
+
+* `randomQuoteIndex(int <min>, int <max>)`: this method returns a random number between min and max, this number used to get a random quote.
+* `readJson(String <path>)`: this method takes in the path of a json file and returns an array of type Quote.
+* `isFound(Quote[] <quotes>, Quote <quote>)`: this method checks if a _quote_ exists in _quotes_. ([AppTest.java](app/src/test/java/quotes/AppTest.java))
+
+### stretch goals
+ 
+to make the user enter the author name from the terminal as an argument and prints the quote for that author, the follwoing methods get that done:
+* `makeAuthorName(String[] <args>)`: since most of the authors have two or more syllable in their name, this method helps getting the right name with no spaces and return it as a string to use by the next method
+* `getQuote(Quote[] <quotes>, String <author>)`: this methods takes in an author name and return a quote for that author if he is in the list, else it will return null.
+
+## Run the code from terminal 
+you need to pass the author name as an argument to the code.
+
+**the author name must be written in quotes**
+
+example:
+
+`./gradlew run --args 'Ron Swanson'`
+
--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -3,8 +3,73 @@
  */
 package quotes;
 
+import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
+        String path = "app/src/main/resources/recentquotes.json";
+        if (System.getProperty("user.dir").endsWith("p"))
+            path = "../" + path;
+
+        Quote[] quotes = readJson(path);
+
+        if (args.length > 0){
+            String author = makeAuthorName(args);
+            String quote = getQuote(quotes, author);
+            if(quote!= null)
+                System.out.println("\u001B[32mQuote by "+ author + " is: " + quote);
+            else
+                System.out.println("\u001B[31mThere is no such author '" + author + "'");
+        }
+
+        int randomIndex = randomQuoteIndex(0, quotes.length);
+        System.out.println();
+        System.out.println("\u001B[35mRandom Quote");
+        System.out.println("\u001B[0m"+quotes[randomIndex]);
 
     }
+
+    public static int randomQuoteIndex(int min, int max){
+        return (int) (Math.random() * max) + min;
+    }
+
+    public static Quote[] readJson(String path){
+        Gson gson = new Gson();
+        Quote[] quotes = null;
+        try{
+            FileReader reader = new FileReader(path);
+            quotes = gson.fromJson(reader, Quote[].class);
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        return quotes;
+    }
+    public static boolean isFound(Quote[] quotes, Quote quote){
+        for(Quote q: quotes){
+            if(q.getAuthor().equals(quote.getAuthor()))
+                return true;
+        }
+        return false;
+    }
+
+    public static String getQuote(Quote[] quotes,String author){
+        for (Quote quote: quotes){
+            if (quote.getAuthor().equals(author))
+                return quote.getText();
+        }
+        return null;
+    }
+
+    public static String makeAuthorName(String[] args){
+        String name = "";
+        for(String word: args){
+            name+=word +" ";
+        }
+        return name.substring(0, name.length()-1);
+    }
+
 }
